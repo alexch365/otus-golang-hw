0aa67efb9acf9676c8cc84ce089b4caa85c6cfbd
Alexander Chapyshev <alexch365@yandex.ru>
2020-03-13 20:24:29 +0300

    HW1 is completed (#1)

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	fmt.Println("current time:", time.Now())
+	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		log.Fatalln("exact time:", err)
+	} else {
+		fmt.Println("exact time:", exactTime)
+	}
 }
