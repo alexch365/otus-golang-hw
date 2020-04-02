232a1a72d5191b26c01539d892f88a902b1aa4a8
Alexander Chapyshev <alexch365@yandex.ru>
2020-04-02 13:30:48 +0300

    Hw3 is completed (#3)

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,40 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+func Top10(str string) []string {
+	if str == "" {
+		return []string{}
+	}
+
+	wordCount := map[string]int{}
+	words := regexp.MustCompile(`\s`).Split(str, -1)
+	for _, word := range words {
+		word = regexp.MustCompile(`(^['"“\-]|['"”!,.:;\-]+?$)`).ReplaceAllString(word, "")
+		if word != "" {
+			wordCount[strings.ToLower(word)]++
+		}
+	}
+
+	type wf struct {
+		Word      string
+		Frequency int
+	}
+	wordFrequencies := make([]wf, 0, len(wordCount))
+	for word, count := range wordCount {
+		wordFrequencies = append(wordFrequencies, wf{word, count})
+	}
+	sort.Slice(wordFrequencies, func(i, j int) bool {
+		return wordFrequencies[i].Frequency > wordFrequencies[j].Frequency
+	})
+
+	topWords := make([]string, 0, 10)
+	for _, freq := range wordFrequencies[:10] {
+		topWords = append(topWords, freq.Word)
+	}
+
+	return topWords
 }
