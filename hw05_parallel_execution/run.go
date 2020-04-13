395c1e42cb974962bc35a11821a0680a56761ad8
Александр Чапышев <alexch365@yandex.ru>
2020-04-13 23:11:58 +0300

    HW05 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,78 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+func worker(tasks <-chan Task, errors chan error) {
+	for task := range tasks {
+		err := task()
+		if err != nil {
+			select {
+			case errors <- err:
+			default:
+				return
+			}
+		}
+	}
+}
+
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
-func Run(tasks []Task, N int, M int) error {
-	// Place your code here
-	return nil
+func Run(tasks []Task, N int, M int) error { //nolint:gocritic
+	if M <= 0 {
+		SimpleRun(tasks, N)
+		return nil
+	}
+
+	tasksCh := make(chan Task, len(tasks))
+	errorsCh := make(chan error, M)
+
+	var wg sync.WaitGroup
+	wg.Add(N)
+	for i := 0; i < N; i++ {
+		go func() {
+			worker(tasksCh, errorsCh)
+			wg.Done()
+		}()
+	}
+
+	for _, task := range tasks {
+		tasksCh <- task
+	}
+	close(tasksCh)
+
+	wg.Wait()
+
+	select {
+	case errorsCh <- errors.New(""):
+		return nil
+	default:
+		return ErrErrorsLimitExceeded
+	}
+}
+
+func SimpleRun(tasks []Task, N int) { //nolint:gocritic
+	tasksCh := make(chan Task, len(tasks))
+
+	var wg sync.WaitGroup
+	wg.Add(N)
+	for i := 0; i < N; i++ {
+		go func() {
+			for task := range tasksCh {
+				_ = task()
+			}
+			wg.Done()
+		}()
+	}
+
+	for _, task := range tasks {
+		tasksCh <- task
+	}
+	close(tasksCh)
+
+	wg.Wait()
 }
