672187ca61a23b6f5e11360c96317abf22d09dd8
Александр Чапышев <alexch365@yandex.ru>
2020-05-10 16:40:30 +0300

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +15,51 @@ var (
 )
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	source, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	sourceStat, err := source.Stat()
+	if err != nil {
+		return err
+	}
+
+	if !sourceStat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	sourceSize := sourceStat.Size()
+	if offset > sourceSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if _, err := source.Seek(offset, 0); err != nil {
+		return err
+	}
+
+	dest, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	if limit == 0 || limit > sourceSize {
+		limit = sourceSize
+	}
+	if (sourceSize - offset) < limit {
+		limit = sourceSize - offset
+	}
+
+	var refreshRate time.Duration = 10
+	bar := pb.Start64(limit).SetRefreshRate(time.Millisecond * refreshRate)
+	defer bar.Finish()
+
+	sourceReader := bar.NewProxyReader(source)
+	if _, err := io.CopyN(dest, sourceReader, limit); err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,10 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/alexch365/otus-golang-hw/hw07_file_copying
 
 go 1.14
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	github.com/cheggaaa/pb/v3 v3.0.4
+	github.com/stretchr/testify v1.5.1
+	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
+)
