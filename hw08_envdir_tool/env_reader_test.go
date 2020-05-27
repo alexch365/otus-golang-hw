4129f033225757b73c23b7b2358854210a04d688
Alexander Chapyshev <alexch365@yandex.ru>
2020-05-27 20:32:40 +0400

    HW08 is completed (#8)

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,45 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	testDir := "testdata/env"
+	testFiles, _ := ioutil.ReadDir(testDir)
+
+	t.Run("skip file with '=' in name", func(t *testing.T) {
+		tempfile, _ := ioutil.TempFile(testDir, "S=KIP")
+		defer os.Remove(tempfile.Name())
+
+		envMap, err := ReadDir(testDir)
+		require.Nil(t, err)
+		require.Equal(t, len(testFiles), len(envMap))
+	})
+
+	t.Run("remove whitespaces and tabs from env var", func(t *testing.T) {
+		tempfile, _ := ioutil.TempFile(testDir, "tabbed lines")
+		defer os.Remove(tempfile.Name())
+
+		ioutil.WriteFile(tempfile.Name(), []byte("env value\t is\x00 \t \nskipped"), os.ModePerm)
+		envMap, err := ReadDir(testDir)
+		require.Nil(t, err)
+		require.Equal(t, len(testFiles)+1, len(envMap))
+		require.Equal(t, "env value\t is\n", envMap[filepath.Base(tempfile.Name())])
+	})
+
+	t.Run("convert empty file to empty string env value", func(t *testing.T) {
+		tempfile, _ := ioutil.TempFile(testDir, "empty")
+		defer os.Remove(tempfile.Name())
+
+		envMap, err := ReadDir(testDir)
+		require.Nil(t, err)
+		require.Equal(t, len(envMap), len(testFiles)+1)
+		require.Equal(t, "", envMap[filepath.Base(tempfile.Name())])
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here
+	minArgsLen := 3
+	if len(os.Args) < minArgsLen {
+		log.Fatalln("Two or more arguments needed. Usage: ./go-envdir /path/to/env/dir command arg1 arg2")
+	}
+
+	envDir := os.Args[1]
+	cmdAndArgs := os.Args[2:]
+	env, err := ReadDir(envDir)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	os.Exit(RunCmd(cmdAndArgs, env))
 }
